f94699e203eb3633298a905f93e290358dd7ace3
Matt Gerard <matt@matthewgerard.com>
2018-12-28 16:48:09 -0800

    initial commit for splash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import vid from './Loop2optomized.mp4';
+import discord from './Discord-Logo+Wordmark-White.png';
+import styled from 'styled-components';
+
+const DiscordLink = styled.a`
+  position: absolute;
+  padding: 20px;
+  display: block;
+  z-index: 2;
+  img {
+    position: relative;
+    width: 300px;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <DiscordLink href="https://discord.gg/bt" target="_self">
+          <img src={discord} alt="discord" />
+        </DiscordLink>
+        <video autoPlay={true} loop={true} src={vid} />
       </div>
     );
   }
